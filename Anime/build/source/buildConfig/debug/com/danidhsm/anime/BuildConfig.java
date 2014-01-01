94a04a4c8d2ace54c965a88031f040056bccaddf
Daniel Hermógenes Sánchez Merlo <danidhsm@hotmail.com>
2014-01-01 04:10:53 +0100

    mejora de aplicacion con hilos

--- Anime/build/source/buildConfig/debug/com/danidhsm/anime/BuildConfig.java
+++ Anime/build/source/buildConfig/debug/com/danidhsm/anime/BuildConfig.java
@@ -1,7 +1,12 @@
-/** Automatically generated file. DO NOT MODIFY */
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
 package com.danidhsm.anime;
 
 public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.danidhsm.anime";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
 }
